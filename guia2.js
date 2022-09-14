254e69f708a381627174e5f4d11e83d582005df6
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-09-14 18:26:10 -0500

    ejercicio 4

--- guia2.js
+++ guia2.js
@@ -1,10 +1,13 @@
 addEventListener("DOMContentLoaded", ()=>{
-   let t= Number(prompt("ingrese el tiempo")) 
-   let h= Number(prompt("ingrese la altura")) 
-   let r= Number(prompt("ingrese el radio")) 
-   let volumen=Math.PI*(r**2)*h 
-   let Q= volumen/t 
-   console.log(`el volumen es =${volumen}`) 
-   console.log(`el caudal es=${Q}`)
-
+   // Un jefe de obra con el dinero que tiene compra cinco juegos de llaves hexagonales cada uno por
+   // $11500. Una bomba en $1168000 y tres cajas de pernos cada uno por $87000. Después de pagar le
+   // sobran $91000. ¿cuánto dinero tenía?
+let llave=11500
+let bomba=1168000
+let caja=87000
+let tllaves=llave*5
+let tcajas=caja*3
+let tpago=tllaves+bomba+tcajas
+console.log(`el dinero  total que pago=${tpago}`)
+console.log(`dinero total que tenia=${tpago+91000}`)
 })
