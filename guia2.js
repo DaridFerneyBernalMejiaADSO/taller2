70c5cfef47f06cf8d3ddcb6c3468ec444281968d
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-09-14 18:29:10 -0500

    ejercicio 5

--- guia2.js
+++ guia2.js
@@ -1,13 +1,9 @@
 addEventListener("DOMContentLoaded", ()=>{
-   // Un jefe de obra con el dinero que tiene compra cinco juegos de llaves hexagonales cada uno por
-   // $11500. Una bomba en $1168000 y tres cajas de pernos cada uno por $87000. Después de pagar le
-   // sobran $91000. ¿cuánto dinero tenía?
-let llave=11500
-let bomba=1168000
-let caja=87000
-let tllaves=llave*5
-let tcajas=caja*3
-let tpago=tllaves+bomba+tcajas
-console.log(`el dinero  total que pago=${tpago}`)
-console.log(`dinero total que tenia=${tpago+91000}`)
+   // En el programa de cocina de “Doña Anita” han dado la receta para la preparación de bizcocho especial
+   // de chocolate. Por cada 100 gramos de harina hay que añadir 10 gramos de cacao y un puñado de
+   // nueces. Si quiero prepararlos con 20 gramos de chocolate. La cantidad de gramos de harina para
+   // hacer el bizcocho es
+let gc=20
+let ph=gc*10
+console.log(`la cantidad de gramos de harina para hacer el bizcocho es=${ph}`)
 })
