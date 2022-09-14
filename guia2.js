8c1e8e96d1f805e7f74e96003717dc05c24bfc73
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-09-14 18:42:45 -0500

    ejercicio 8

--- guia2.js
+++ guia2.js
@@ -1,13 +1,9 @@
 addEventListener("DOMContentLoaded", ()=>{
-   // tres estudiantes reunieron tapas de gaseosas para repartirlas y venderlas al final del año. Martin toma
-   // 2/3 del total. Jairo un cuarto del total, y Lorena se queda con el resto. ¿qué parte le corresponde a
-   // Lorena?
-   let dr= Number(prompt("tapas recojidas"))
-   let martin=(2/3)*dr
-   let jairo=dr/4
-   let mjr=martin+jairo
-   let lorena=dr-mjr
-   console.log(`la parte que le correspondio a lorena es=${lorena}`)
-   
+   // Pinturas “La brocha gorda” requiere determinar cuánto cobrar por trabajos de pintura. Considere que
+   // se cobra por m2 y realice el código que representen el algoritmo que le permita ir generando
+   // presupuestos para cada cliente.
+let m2= Number(prompt("precio del metro cuadrado"))
+let tm= Number(prompt("cuantos metros son"))
+console.log(`el trabajo para su pintura es de=${m2*tm}`)
    
 })
