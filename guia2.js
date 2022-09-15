9310f03372eab40c2cfa58a914aded56ebb2bf2c
DaridFerneyBernalMejiaADSO <daridbernal2004@gmail.com>
2022-09-14 19:16:16 -0500

    ejercicio 10

--- guia2.js
+++ guia2.js
@@ -1,13 +1,9 @@
 addEventListener("DOMContentLoaded", ()=>{
-   // . Una empresa que contrata personal requiere determinar la edad de las personas que solicitan trabajo,
-   // pero cuando se les realiza la entrevista sólo se les pregunta el año en que nacieron. Realice el código
-   // que representen el algoritmo para solucionar este problema.
-   // Al final debe entregar el nombre de la persona entrevistada en mayúscula sostenida y minunscula
-   // sostenida y la edad del entrevistado.
-let nombre=(prompt("digite su nombre="))
-let nacimiento=Number(prompt("su año de nacimiento "))
-let añoactual=2022
-console.log(`persona entrevistada=${nombre}`)
-console.log(`edad de la personja entrevistada=${añoactual-nacimiento}`)
+   //frase que lo identifique como programador de Software
+let texto = " La ciencia de hoy es la tecnologia del mañana "
+console.log(texto)
+const invertirCadena = texto => texto.split("").reverse().join("");
+ 
+console.log( invertirCadena(texto));
    
 })
